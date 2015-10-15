bae9d991c14fa19bceeeecea4482d2b717c1fa22
oricalvo <ori.calvo@gmail.com>
2015-10-15 15:20:27 +0300

    More

--- 08 JavaScript/AngularWithTypescript/AngularWithTypescript/HomeCtrl.js
+++
@@ -1,7 +0,0 @@
-var HomeCtrl = (function () {
-    function HomeCtrl() {
-        this.message = "Hell123o Ng+TS";
-    }
-    return HomeCtrl;
-})();
-angular.module("MyApp").controller("HomeCtrl", HomeCtrl);
--- 08 JavaScript/AngularWithTypescript/AngularWithTypescript/HomeCtrl.ts
+++
@@ -1,9 +0,0 @@
-﻿class HomeCtrl {
-    message: string;
-
-    constructor() {
-        this.message = "Hell123o Ng+TS";
-    }
-}
-
-angular.module("MyApp").controller("HomeCtrl", HomeCtrl);
